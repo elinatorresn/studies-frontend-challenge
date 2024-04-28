d5a1e00af0d5883015ab609c01e98aa61a6c2bd0
Elina Torres <elinatorresn@gmail.com>
2024-04-28 11:42:33 -0300

    logo and header

--- capputeeno/src/components/header.tsx
+++ capputeeno/src/components/header.tsx
@@ -1,4 +1,13 @@
-import styled from "styled-components"
+"use client"
+
+import { styled } from "styled-components"
+
+import { Saira_Stencil_One } from "next/font/google";
+
+const sairaStencil = Saira_Stencil_One({ 
+  weight: ['400'],
+  subsets: ['latin'] 
+});
 
 interface HeaderProps {
 
@@ -8,14 +17,21 @@ const TagHeader = styled.header`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    paddin: 20px 160px;
+    padding: 20px 160px;
 `
 
-export function(props : HeaderProps){
+const Logo = styled.a`
+    color: var(--logo-color);
+    font-weight: 400;
+    font-size: 2.5rem;
+    line-height: 150%;
+`
+
+export function Header(props : HeaderProps){
     return(
-        <header>
+        <TagHeader>
+            <Logo className={sairaStencil.className}>Capputeeno</Logo>
             <div></div>
-            <div></div>
-        </header>
+        </TagHeader>
     )
 }
