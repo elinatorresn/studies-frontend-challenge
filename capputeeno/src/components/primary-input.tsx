e5143879b0d37f8ac9c90b5f116f2c0aedf10835
Elina Torres <elinatorresn@gmail.com>
2024-04-28 14:22:05 -0300

    search input and cart button in header

---
+++ capputeeno/src/components/primary-input.tsx
@@ -0,0 +1,41 @@
+import { styled } from "styled-components"
+import { SearchIcon } from "./search-icon"
+import { InputHTMLAttributes } from "react"
+
+export const PrimaryInput = styled.input`
+    width: 352px;
+    border-radius: 0.5rem;
+    border: none;
+    padding: 10px 16px;
+    font-family: inherit;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+    
+    background-color: var(--bg-search-input);
+    color: var(--text-dark);
+`
+
+const InputContainer = styled.div`
+    position: relative;
+    width: 352px;
+
+    svg{
+        position: absolute;
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+`
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function PrimaryInputWSearchIcon(props: InputProps){
+    return (
+        <InputContainer>
+            <PrimaryInput {...props}/>
+            <SearchIcon/>
+        </InputContainer>
+    )
+}
+
